197fc4d6893f04c0701c52e2c2f5c090fbe7e812
Admin <admin@Mac.local>
2022-04-05 07:50:35 +0300

    first commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var buttonZero: UIButton!
     
     @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var buttonChangeResultLabel: UIStepper!
     @IBOutlet weak var buttons: UIStackView!
     
     var numberOne = ""
@@ -31,17 +32,72 @@ class ViewController: UIViewController {
     @IBAction func inputNumber(_ sender: UIButton) {
         
         if operand.isEmpty {
-            numberOne = numberOne +
-                {sender.titleLabel?.text}!
+            numberOne = numberOne + {sender.titleLabel?.text}!
             resultLabel.text = numberOne
         }
         else
         {
-            numberTwo = numberTwo +
-            {sender.titleLabel?.text}!
+            numberTwo = numberTwo + {sender.titleLabel?.text}!
             resultLabel.text = numberTwo
         }
     }
     
+    
+    @IBAction func inputOperand(_ sender: UIButton) {
+        operand = sender.titleLabel?.text as!
+        String
+    }
+    
+    
+    @IBAction func clearAction(_ sender: UIButton) {
+        numberTwo = ""
+        numberOne = ""
+        operand = ""
+        resultLabel.text = "0"
+    }
+    
+    
+    @IBAction func resultAction(_ sender: UIButton) {
+        
+        var result = 0.0
+        
+        switch operand {
+        case "/":
+            result = Double (numberOne)! /
+                Double(numberTwo)!
+        case "+":
+            result = Double (numberOne)! +
+                Double(numberTwo)!
+        case "-":
+            result = Double (numberOne)! -
+                Double(numberTwo)!
+        case "*":
+            result = Double (numberOne)! *
+                Double(numberTwo)!
+        default:
+            break
+        }
+        
+        if result.truncatingRemainder(dividingBy: 1.0)
+            == 0.0 {
+            resultLabel.text = String(Int(result))
+        }
+        else {
+            resultLabel.text = String(result)
+        }
+    }
+    
+    
+    @IBAction func changeResultLabel(_ sender: UIStepper) {
+        let font = resultLabel.font?.fontName
+        let fontSize = CGFloat(sender.Value)
+        
+        resultLabel.font = UIFont(name: font!, size: fontSize)
+    }
+    
+    @IBAction func hiddenButtons(_sender: Any) {
+        buttons.isHidden = !buttons.isHidden
+        buttonChangeResultLabel.isHidden = !buttonChangeResultLabel.isHidden
+    }
 }
 
